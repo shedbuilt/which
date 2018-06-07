b02235bfa1345e5d87fd1ff458801500d3e81af5
Auston Stewart <auston@shedbuilt.net>
2018-06-07 08:11:18 -1000

    Updating build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-./configure --prefix=/usr
-make -j $SHED_NUM_JOBS
-make DESTDIR=${SHED_FAKE_ROOT} install
+./configure --prefix=/usr &&
+make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" install
